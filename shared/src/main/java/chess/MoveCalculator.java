d47b99184b400a028ab3dcebd357147a3ee8ce6b
Brandon Jacobs <jacobs27@student.byu.edu>
2024-01-20 15:46:14 -0700

    Passed Knight Tests

--- shared/src/main/java/chess/MoveCalculator.java
+++ shared/src/main/java/chess/MoveCalculator.java
@@ -6,7 +6,7 @@ public class MoveCalculator {
     
     public MoveCalculator () {}
     
-    public ArrayList<ChessMove> calculateMoves(ChessPosition[] coordinateChanges, ChessPosition startPosition) {
+    public static ArrayList<ChessMove> calculateMoves(ArrayList<ChessPosition> coordinateChanges, ChessPosition startPosition, ChessBoard board, ChessGame.TeamColor startColor) {
         ArrayList<ChessMove> moves = new ArrayList<>();
         int startRow = startPosition.getRow();
         int startCol = startPosition.getColumn();
@@ -14,24 +14,29 @@ public class MoveCalculator {
             int endRow = c.getRow() + startRow;
             int endCol = c.getColumn() + startCol;
 
-            if (endRow >= 1 && endRow <= 8 && endCol >= 1 && endCol <= 8) {
-                moves.add(new ChessMove(startPosition, new ChessPosition(endRow, endCol), null));
+            if (endRow >= 1 && endRow <= 8 && endCol >= 1 && endCol <= 8) { // Checks that move is in bounds
+                ChessPiece endPiece = board.getPiece(new ChessPosition(endRow, endCol));
+                if (endPiece == null) {
+                    moves.add(new ChessMove(startPosition, new ChessPosition(endRow, endCol), null));
+                } else if (endPiece.teamColor != startColor) {
+                    moves.add(new ChessMove(startPosition, new ChessPosition(endRow, endCol), null));
+                }
             }
         }
         return moves;
     }
 
-    public ArrayList<ChessPosition> rookCoordinateChanges() {
+    public static ArrayList<ChessPosition> rookCoordinateChanges() {
         ArrayList<ChessPosition> positions = new ArrayList<>();
         for (int i = 0; i < 8; i++) {
             positions.add(new ChessPosition(0, i));
             positions.add(new ChessPosition(0, i * -1));
-            positions.add(new ChessPosition(0, i));
-            positions.add(new ChessPosition(0, i * -1));
+            positions.add(new ChessPosition(i, 0));
+            positions.add(new ChessPosition(i * -1, 0));
         }
         return positions;
     }
-    public ArrayList<ChessPosition> knightCoordinateChanges() {
+    public static ArrayList<ChessPosition> knightCoordinateChanges() {
         ArrayList<ChessPosition> positions = new ArrayList<>();
 
         positions.add(new ChessPosition(1, 2));
@@ -46,25 +51,25 @@ public class MoveCalculator {
         return positions;
     }
 
-    public ArrayList<ChessPosition> bishopCoordinateChanges() {
+    public static ArrayList<ChessPosition> bishopCoordinateChanges() {
         ArrayList<ChessPosition> positions = new ArrayList<>();
 
         return positions;
 
     }
-    public ArrayList<ChessPosition> kingCoordinateChanges() {
+    public static ArrayList<ChessPosition> kingCoordinateChanges() {
         ArrayList<ChessPosition> positions = new ArrayList<>();
 
         return positions;
 
     }
-    public ArrayList<ChessPosition> queenCoordinateChanges() {
+    public static ArrayList<ChessPosition> queenCoordinateChanges() {
         ArrayList<ChessPosition> positions = new ArrayList<>();
 
         return positions;
 
     }
-    public ArrayList<ChessPosition> pawnCoordinateChanges() {
+    public static ArrayList<ChessPosition> pawnCoordinateChanges() {
         ArrayList<ChessPosition> positions = new ArrayList<>();
 
         return positions;
