f2e4dfab5688e48a02b240dc0e711d3bd6ab1f82
Brandon Jacobs <jacobs27@student.byu.edu>
2024-01-20 12:29:47 -0700

    Added toString method

--- shared/src/main/java/chess/ChessPosition.java
+++ shared/src/main/java/chess/ChessPosition.java
@@ -33,6 +33,12 @@ public class ChessPosition {
     }
 
     @Override
+    public String toString() {
+        return String.format("(%d, %d)", row, col);
+    }
+
+
+    @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
