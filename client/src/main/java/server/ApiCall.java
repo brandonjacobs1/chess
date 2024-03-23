c56dfae686653f9a3769a4ae4180a9b1aee92da5
Brandon Jacobs <jacobs27@student.byu.edu>
2024-03-22 23:02:43 -0600

    refactor tests

---
+++ client/src/main/java/server/ApiCall.java
@@ -0,0 +1,73 @@
+package server;
+
+import com.google.gson.Gson;
+import model.AuthData;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.lang.reflect.Type;
+import java.net.HttpURLConnection;
+import java.net.URI;
+import java.net.URL;
+
+public class ApiCall {
+    private final String serverUrl;
+
+    public ApiCall(String url) {
+        serverUrl = url;
+    }
+    public <T> T makeRequest(String method, String path, Object request, Type responseClass, AuthData auth) throws ResponseException {
+        try {
+            URL url = (new URI(serverUrl + path)).toURL();
+            HttpURLConnection http = (HttpURLConnection) url.openConnection();
+            http.setRequestMethod(method);
+            if (auth != null) http.addRequestProperty("authorization", auth.authToken());
+            http.setDoOutput(true);
+
+            writeBody(request, http);
+            http.connect();
+            throwIfNotSuccessful(http);
+            return readBody(http, responseClass);
+        } catch (Exception ex) {
+            throw new ResponseException(500, ex.getMessage());
+        }
+    }
+
+
+    public static void writeBody(Object request, HttpURLConnection http) throws IOException {
+        if (request != null) {
+            http.addRequestProperty("Content-Type", "application/json");
+            String reqData = new Gson().toJson(request);
+            try (OutputStream reqBody = http.getOutputStream()) {
+                reqBody.write(reqData.getBytes());
+            }
+        }
+    }
+
+    public void throwIfNotSuccessful(HttpURLConnection http) throws IOException, ResponseException {
+        var status = http.getResponseCode();
+        if (!isSuccessful(status)) {
+            throw new ResponseException(status, "failure: " + status);
+        }
+    }
+
+    public static <T> T readBody(HttpURLConnection http, Type responseClass) throws IOException {
+        T response = null;
+        if (http.getContentLength() < 0) {
+            try (InputStream respBody = http.getInputStream()) {
+                InputStreamReader reader = new InputStreamReader(respBody);
+                if (responseClass != null) {
+                    response = new Gson().fromJson(reader, responseClass);
+                }
+            }
+        }
+        return response;
+    }
+
+
+    public boolean isSuccessful(int status) {
+        return status / 100 == 2;
+    }
+}
--- client/src/main/java/server/ServerFacade.java
+++ client/src/main/java/server/ServerFacade.java
@@ -15,101 +15,47 @@ import model.JoinGameBody;
 import model.UserData;
 
 public class ServerFacade {
-
-    private final String serverUrl;
+    private ApiCall apiManager;
 
     public ServerFacade(String url) {
-        serverUrl = url;
+        this.apiManager = new ApiCall(url);
     }
 
 
     public AuthData register(UserData user) throws ResponseException {
         var path = "/user";
         Type t = new TypeToken<AuthData>(){}.getType();
-        return this.makeRequest("POST", path, user, t, null);
+        return apiManager.makeRequest("POST", path, user, t, null);
     }
 
     public AuthData login(UserData user) throws ResponseException {
         var path = "/session";
-        return this.makeRequest("POST", path, user, new TypeToken<AuthData>(){}.getType(), null);
+        return apiManager.makeRequest("POST", path, user, new TypeToken<AuthData>(){}.getType(), null);
     }
 
     public void logout(AuthData auth) throws ResponseException {
         var path = "/session";
-        this.makeRequest("DELETE", path, null, null, auth);
+        apiManager.makeRequest("DELETE", path, null, null, auth);
     }
 
     public Map<String, ArrayList<GameData>> listGames(AuthData auth) throws ResponseException {
         var path = "/game";
         Type t = new TypeToken<Map<String, ArrayList<GameData>>>(){}.getType();
-        return this.makeRequest("GET", path, null, t, auth);
+        return apiManager.makeRequest("GET", path, null, t, auth);
     }
 
     public GameData joinGame(AuthData auth, int gameID, JoinGameBody.Color color) throws ResponseException {
         var path = "/game";
-        return this.makeRequest("PUT", path, new JoinGameBody(color, gameID), null, auth);
+        return apiManager.makeRequest("PUT", path, new JoinGameBody(color, gameID), null, auth);
     }
 
     public GameData createGame(AuthData auth, GameData game) throws ResponseException {
         var path = "/game";
-        return this.makeRequest("POST", path, game, new TypeToken<GameData>(){}.getType(), auth);
+        return apiManager.makeRequest("POST", path, game, new TypeToken<GameData>(){}.getType(), auth);
     }
 
     public void clear() throws ResponseException {
         var path = "/db";
-        this.makeRequest("DELETE", path, null, null, null);
-    }
-
-    private <T> T makeRequest(String method, String path, Object request, Type responseClass, AuthData auth) throws ResponseException {
-        try {
-            URL url = (new URI(serverUrl + path)).toURL();
-            HttpURLConnection http = (HttpURLConnection) url.openConnection();
-            http.setRequestMethod(method);
-            if (auth != null) http.addRequestProperty("authorization", auth.authToken());
-            http.setDoOutput(true);
-
-            writeBody(request, http);
-            http.connect();
-            throwIfNotSuccessful(http);
-            return readBody(http, responseClass);
-        } catch (Exception ex) {
-            throw new ResponseException(500, ex.getMessage());
-        }
-    }
-
-
-    private static void writeBody(Object request, HttpURLConnection http) throws IOException {
-        if (request != null) {
-            http.addRequestProperty("Content-Type", "application/json");
-            String reqData = new Gson().toJson(request);
-            try (OutputStream reqBody = http.getOutputStream()) {
-                reqBody.write(reqData.getBytes());
-            }
-        }
-    }
-
-    private void throwIfNotSuccessful(HttpURLConnection http) throws IOException, ResponseException {
-        var status = http.getResponseCode();
-        if (!isSuccessful(status)) {
-            throw new ResponseException(status, "failure: " + status);
-        }
-    }
-
-    private static <T> T readBody(HttpURLConnection http, Type responseClass) throws IOException {
-        T response = null;
-        if (http.getContentLength() < 0) {
-            try (InputStream respBody = http.getInputStream()) {
-                InputStreamReader reader = new InputStreamReader(respBody);
-                if (responseClass != null) {
-                    response = new Gson().fromJson(reader, responseClass);
-                }
-            }
-        }
-        return response;
-    }
-
-
-    private boolean isSuccessful(int status) {
-        return status / 100 == 2;
+        apiManager.makeRequest("DELETE", path, null, null, null);
     }
 }
