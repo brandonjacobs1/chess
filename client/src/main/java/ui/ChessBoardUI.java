0e726cf8a099689d26b6c69926c59575f32704e7
Brandon Jacobs <jacobs27@student.byu.edu>
2024-04-11 19:15:43 -0600

    fix ChessBoardUI board

--- client/src/main/java/ui/ChessBoardUI.java
+++ client/src/main/java/ui/ChessBoardUI.java
@@ -97,39 +97,25 @@ public class ChessBoardUI {
 
     private String getPieceSymbol(ChessPiece piece) {
         if (piece.getTeamColor() == ChessGame.TeamColor.WHITE) {
-            switch (piece.getPieceType()) {
-                case KING:
-                    return EscapeSequences.WHITE_KING;
-                case QUEEN:
-                    return EscapeSequences.WHITE_QUEEN;
-                case BISHOP:
-                    return EscapeSequences.WHITE_BISHOP;
-                case KNIGHT:
-                    return EscapeSequences.WHITE_KNIGHT;
-                case ROOK:
-                    return EscapeSequences.WHITE_ROOK;
-                case PAWN:
-                    return EscapeSequences.WHITE_PAWN;
-                default:
-                    return EscapeSequences.EMPTY;
-            }
+            return switch (piece.getPieceType()) {
+                case KING -> EscapeSequences.WHITE_KING;
+                case QUEEN -> EscapeSequences.WHITE_QUEEN;
+                case BISHOP -> EscapeSequences.WHITE_BISHOP;
+                case KNIGHT -> EscapeSequences.WHITE_KNIGHT;
+                case ROOK -> EscapeSequences.WHITE_ROOK;
+                case PAWN -> EscapeSequences.WHITE_PAWN;
+                default -> EscapeSequences.EMPTY;
+            };
         } else {
-            switch (piece.getPieceType()) {
-                case KING:
-                    return EscapeSequences.BLACK_KING;
-                case QUEEN:
-                    return EscapeSequences.BLACK_QUEEN;
-                case BISHOP:
-                    return EscapeSequences.BLACK_BISHOP;
-                case KNIGHT:
-                    return EscapeSequences.BLACK_KNIGHT;
-                case ROOK:
-                    return EscapeSequences.BLACK_ROOK;
-                case PAWN:
-                    return EscapeSequences.BLACK_PAWN;
-                default:
-                    return EscapeSequences.EMPTY;
-            }
+            return switch (piece.getPieceType()) {
+                case KING -> EscapeSequences.BLACK_KING;
+                case QUEEN -> EscapeSequences.BLACK_QUEEN;
+                case BISHOP -> EscapeSequences.BLACK_BISHOP;
+                case KNIGHT -> EscapeSequences.BLACK_KNIGHT;
+                case ROOK -> EscapeSequences.BLACK_ROOK;
+                case PAWN -> EscapeSequences.BLACK_PAWN;
+                default -> EscapeSequences.EMPTY;
+            };
         }
     }
 }
